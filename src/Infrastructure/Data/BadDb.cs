51d502f76eb97b0054a8533f57e240967a3c1daf
juancamilop1 <159964422+juancamilop1@users.noreply.github.com>
2025-11-19 19:48:16 -0500

    BadDb

--- src/Infrastructure/Data/BadDb.cs
+++ src/Infrastructure/Data/BadDb.cs
@@ -9,21 +9,22 @@ using System.Data.SqlClient;
 
 public static class BadDb
 {
-    public static string ConnectionString = "Server=localhost;Database=master;User Id=sa;Password=SuperSecret123!;TrustServerCertificate=True";
+	public static string ConnectionString = "Server=localhost;Database=master;User Id=sa;******;TrustServerCertificate=True";
 
 
-    public static int ExecuteNonQueryUnsafe(string sql)
-    {
-        var conn = new SqlConnection(ConnectionString);
-        var cmd = new SqlCommand(sql, conn);
-        conn.Open();
-        return cmd.ExecuteNonQuery();
-    }
+	public static int ExecuteNonQueryUnsafe(string sql)
+	{
+		var conn = new SqlConnection(ConnectionString);
+		var cmd = new SqlCommand(sql, conn);
+		conn.Open();
+		return cmd.ExecuteNonQuery();
+	}
 
-    public static IDataReader ExecuteReaderUnsafe(string sql)
-    {
-        var conn = new SqlConnection(ConnectionString);
-        var cmd = new SqlCommand(sql, conn);
-        conn.Open();
-        return cmd.ExecuteReader(); 
+	public static IDataReader ExecuteReaderUnsafe(string sql)
+	{
+		var conn = new SqlConnection(ConnectionString);
+		var cmd = new SqlCommand(sql, conn);
+		conn.Open();
+		return cmd.ExecuteReader();
+	}
 }
